49eafb33f3c42e2f632520251e1b5a4d3ef320ad
Mihaela <mihaelaflorea201@yahoo.com>
2022-01-29 19:23:22 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// Require dependencies
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// Create app
+const app = express();
+
+// App middleware
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello 🌏!'
+  })
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`)
+});
