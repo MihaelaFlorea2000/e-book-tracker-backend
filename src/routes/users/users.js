6731e31375bb6a1cbe39f7b51c1f10e97162467f
Mihaela <mihaelaflorea201@yahoo.com>
2022-01-30 14:31:54 +0200

    Register users mongo

--- src/routes/users/users.js
+++ src/routes/users/users.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const { pool } = require('../../config/postgresConfig');
+const User = require('../../models/User.js');
 const { PROFILE_IMAGE } = require('../../helpers/constants');
 const { returnMsg } = require('../../helpers/returnMsg');
 
@@ -28,7 +29,7 @@ router.post('/register', async (req, res, next) => {
     return returnMsg(res, 400, false, "Passwords don't match");
   }
 
-  // Check email exists
+  // Check email exists Postgresql
   try {
     const data = await pool.query(
       'SELECT id FROM users WHERE users.email = $1', 
@@ -50,7 +51,7 @@ router.post('/register', async (req, res, next) => {
       next(err)
     } 
 
-    // Add user to db
+    // Add user to PostgreSQL
     try {
       await pool.query(
           'INSERT INTO users (email, password, first_name, last_name, profile_image) VALUES ($1, $2, $3, $4, $5)', 
@@ -60,6 +61,59 @@ router.post('/register', async (req, res, next) => {
     } catch (err) {
       res.status(500);
       next(err)
+    }
+  });
+});
+
+
+router.post('/register/m', async (req, res, next) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  // Check request body is ok
+  if (!email || !password || !confirmPassword || !firstName || !lastName) {
+    return returnMsg(res, 400, false, "Bad Request");
+  }
+
+  // Check passwords match
+  if (password !== confirmPassword) {
+    return returnMsg(res, 400, false, "Passwords don't match");
+  }
+
+  // Check email exists Mongodb
+  try {
+    const user = await User.findOne({email: email});
+
+    if (user !== null) {
+      return returnMsg(res, 400, false, "Email already exists");
+    }
+    
+  } catch (error) {
+    res.status(400);
+    next(error);
+  }
+
+  // Hash password
+  await bcrypt.hash(password, 5, async (err, hash) => {
+    if (err) {
+      res.status(500);
+      next(err)
+    } 
+
+    // Add user Mongodb
+    const user = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hash,
+      profileImage: PROFILE_IMAGE
+    });
+
+    try {
+      await user.save();
+      return returnMsg(res, 201, true, "OK");
+    } catch (error) {
+      res.status(500);
+      next(error);
     }
   });
 });
