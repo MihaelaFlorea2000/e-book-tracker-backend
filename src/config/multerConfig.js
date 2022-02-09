55d7e6fb267c2c18a93ef5cb09770068a7607e10
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-09 20:16:24 +0200

    Empty commit

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -10,7 +10,8 @@ const uploadBookMulter = multer({
 }).fields([
   { name: 'file', maxCount: 1 },
   { name: 'coverImage', maxCount: 1 }
-]);
+]); 
+
 
 const bucket = uploadBookStorage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
