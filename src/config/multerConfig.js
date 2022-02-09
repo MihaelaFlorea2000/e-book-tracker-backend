a27ba53955d6ce3728c38d8663970c4354444572
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-09 21:23:26 +0200

    Changed gcs cloud config

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,13 +1,8 @@
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
 const {Storage} = require('@google-cloud/storage'); 
 
 // Upload Book
-const uploadBookStorage = new Storage({
-  projectId: 'comp3200',
-  credentials: process.env.NODE_ENV === 'development' ? process.env.GOOGLE_APPLICATION_CREDENTIALS : process.env.GOOGLE_CREDENTIALS
-});
+const uploadBookStorage = new Storage();
 const uploadBookMulter = multer({
   storage: multer.memoryStorage()
 }).fields([
