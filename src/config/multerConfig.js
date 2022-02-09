efb37cbbbe5de2eded8443af2eb24eb5c5c8ce19
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-09 21:15:39 +0200

    Added gcs credentials

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -1,10 +1,13 @@
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const {Storage} = require('@google-cloud/storage');
+const {Storage} = require('@google-cloud/storage'); 
 
 // Upload Book
-const uploadBookStorage = new Storage();
+const uploadBookStorage = new Storage({
+  projectId: 'comp3200',
+  credentials: process.env.NODE_ENV === 'development' ? process.env.GOOGLE_APPLICATION_CREDENTIALS : process.env.GOOGLE_CREDENTIALS
+});
 const uploadBookMulter = multer({
   storage: multer.memoryStorage()
 }).fields([
