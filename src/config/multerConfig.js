a16481c1cef55f31968e4e3b27ca342b5f084206
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-15 11:52:26 +0200

    CORS config for GCS

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -10,7 +10,37 @@ const uploadBookMulter = multer({
   { name: 'coverImage', maxCount: 1 }
 ]); 
 
+// The origin for this CORS config to allow requests from
+const origin = process.env.CORS_ORIGIN;
 
+// The response header to share across origins
+const responseHeader = 'Content-Type';
+
+// The maximum amount of time the browser can make requests before it must
+// repeat preflighted requests
+const maxAgeSeconds = 3600;
+
+// The name of the method
+const method = 'GET';
+
+(async () => {
+  await configureBucketCors();
+})();
 const bucket = uploadBookStorage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
+async function configureBucketCors() {
+  await uploadBookStorage.bucket(process.env.GCLOUD_STORAGE_BUCKET).setCorsConfiguration([
+    {
+      maxAgeSeconds,
+      method: [method],
+      origin: [origin],
+      responseHeader: [responseHeader],
+    },
+  ]);
+
+  console.log(`Bucket was updated with a CORS config
+      to allow ${method} requests from ${origin} sharing 
+      ${responseHeader} responses across origins`);
+}
 
 module.exports = { uploadBookMulter, bucket }
