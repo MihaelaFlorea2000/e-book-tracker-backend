b35aa26abff389b863680c87649010d31e94fadb
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-02-24 18:46:47 +0200

    Show reads ordered by date

--- src/routes/pg/books/reads/reads.js
+++ src/routes/pg/books/reads/reads.js
@@ -11,7 +11,7 @@ router.get('/', authenticateToken, async (req, res, next) => {
 
   try {
     const data = await pool.query(
-      'SELECT id, start_date AS "startDate", end_date AS "endDate", rating, notes, time, sessions FROM reads WHERE book_id = $1 AND user_id = $2;',
+      'SELECT id, start_date AS "startDate", end_date AS "endDate", rating, notes, time, sessions FROM reads WHERE book_id = $1 AND user_id = $2 ORDER BY end_date DESC;',
       [bookId, user.id]
     )
     res.status(200).json(data.rows);
