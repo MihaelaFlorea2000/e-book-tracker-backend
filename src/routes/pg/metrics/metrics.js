9cd5b1ed38b2507d871494da057da22e2639b5d7
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-03-02 19:29:36 +0200

    Bug fix in goals

--- src/routes/pg/metrics/metrics.js
+++ src/routes/pg/metrics/metrics.js
@@ -123,7 +123,7 @@ router.get('/goals', authenticateToken, async (req, res, next) => {
 
     // Minutes and hours read per day
     const hoursPerDay = dailyTime.rows[0].count.hours ? dailyTime.count.rows[0].hours : 0;
-    const minutesPerDay = dailyTime.rows[0].count.minutes ? dailyTime.count.rows[0].minutes : 0;
+    const minutesPerDay = dailyTime.rows[0].count.minutes ? dailyTime.rows[0].count.minutes : 0;
 
     // Total user daily goal in minutes
     const setDaily = setGoals.rows[0].dailyHours * 60 + setGoals.rows[0].dailyMinutes;
