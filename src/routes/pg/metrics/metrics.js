b482c63c589950f255181d0d0863e3bfb49059b3
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-03-04 18:37:11 +0200

    Test date format

--- src/routes/pg/metrics/metrics.js
+++ src/routes/pg/metrics/metrics.js
@@ -328,11 +328,15 @@ router.get('/calendar', authenticateToken, async (req, res, next) => {
       [user.id]
     )
 
+    console.log(days.rows);
+
     const dates = [];
 
     days.rows.forEach((row) => {
-      dates.push(new Date(row.date).toLocaleDateString('en-UK'));
+      dates.push(new Date(row.date).toLocaleDateString());
     })
+
+    //console.log(dates);
 
     res.status(200).json(dates);
   } catch (err) {
