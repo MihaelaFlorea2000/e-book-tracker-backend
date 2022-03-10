b09d0c4f423a16c8273e353e128b597b01abc093
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-03-10 13:58:07 +0200

    Gettings reads for any user

--- src/routes/pg/reads/reads.js
+++ src/routes/pg/reads/reads.js
@@ -10,12 +10,11 @@ const readIdRouter = require('./readId');
 // Get the reads of a specific book
 router.get('/', authenticateToken, async (req, res, next) => {
   const bookId = req.params.bookId;
-  const user = req.user;
 
   try {
     const data = await pool.query(
-      'SELECT id, start_date AS "startDate", end_date AS "endDate", rating, notes FROM reads WHERE book_id = $1 AND user_id = $2 ORDER BY end_date DESC;',
-      [bookId, user.id]
+      'SELECT id, start_date AS "startDate", end_date AS "endDate", rating, notes FROM reads WHERE book_id = $1 ORDER BY end_date DESC;',
+      [bookId]
     );
 
     const readsData = [];
