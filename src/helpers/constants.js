514f369ac9357f1938c61cfd6e0ce5524f46b95a
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2022-03-28 09:22:52 +0300

    Changed default profile image

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
-const PROFILE_IMAGE = "https://images.unsplash.com/photo-1440508319978-8b67875e39d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2367&q=80";
+const PROFILE_IMAGE = "https://storage.googleapis.com/comp3200-uploads/defaultProfileImage.jpg";
 
 const COVER_IMAGE = "https://images.unsplash.com/photo-1528459105426-b9548367069b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1328&q=80";
 
 const START_LOCATION = "epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)"
 
-module.exports = { PROFILE_IMAGE, COVER_IMAGE, START_LOCATION  }
+module.exports = { PROFILE_IMAGE, COVER_IMAGE, START_LOCATION }
